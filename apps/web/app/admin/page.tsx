0e29855b499941ef634e279e1ca74340986a3bb1
Jaimelxrd <jf847852208@gmail.com>
2026-06-12 14:43:52 +0200

    Update page.tsx

--- apps/web/app/admin/page.tsx
+++ apps/web/app/admin/page.tsx
@@ -1,6 +1,5 @@
 import { prisma } from "@enso/database"
 import { getServerSession } from "next-auth"
-import { authOptions } from "@/lib/auth"
 import { redirect } from "next/navigation"
 import Link from "next/link"
 
