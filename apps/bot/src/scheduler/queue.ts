027b9645ae98a189a55cf6b010bfd39146f90970
lausia <lausiaivan@gmail.com>
2026-06-12 11:52:20 +0200

    Update queue.ts

--- apps/bot/src/scheduler/queue.ts
+++ apps/bot/src/scheduler/queue.ts
@@ -1,7 +1,7 @@
 import { Queue, Worker, QueueEvents } from "bullmq"
 import IORedis from "ioredis"
 
-console.log("REDIS_URL:", process.env.REDIS_URL)
+console.log("REDIS_URL: redis://default:JAiQAOYxrlxnsfHsqthqAuKFdcAdltQC@yamanote.proxy.rlwy.net:53545", process.env.REDIS_URL)
 
 export const connection = new IORedis(
   process.env.REDIS_URL || "redis://localhost:6379",
