aae82edaa814b6b3b1d5e18ac477748f2e6851df
Jaimelxrd <jf847852208@gmail.com>
2026-06-12 14:51:08 +0200

    Update route.ts

--- apps/web/app/api/admin/utilizadores/route.ts
+++ apps/web/app/api/admin/utilizadores/route.ts
@@ -1,6 +1,4 @@
 import { NextRequest, NextResponse } from "next/server"
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/lib/auth"
 import { prisma } from "@enso/database"
 import bcrypt from "bcryptjs"
 
