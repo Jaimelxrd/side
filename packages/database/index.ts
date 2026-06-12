5f2c0d06b5d8522f28fb4289bcd8a99aa8bff98e
JaimeLxrd <jf847852208@gmail.com>
2026-06-12 10:15:29 +0200

    fix: use datasourceUrl for PrismaClient v7

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -6,11 +6,7 @@ const globalForPrisma = globalThis as unknown as {
 
 function getPrismaClient() {
   return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
+    datasourceUrl: process.env.DATABASE_URL,
   })
 }
 
