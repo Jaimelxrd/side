26108fa281a3955387a44f4fc0be8a9797241c8f
JaimeLxrd <jf847852208@gmail.com>
2026-06-12 11:47:19 +0200

    fix: remove PrismaPg adapter to fix build

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -4,19 +4,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-function getPrismaClient() {
-  const { PrismaPg } = require("@prisma/adapter-pg")
-  const { Pool } = require("pg")
-  
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter })
-}
-
 export const prisma: PrismaClient =
-  globalForPrisma.prisma ?? getPrismaClient()
+  globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
