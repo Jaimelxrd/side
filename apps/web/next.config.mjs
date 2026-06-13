9ce62025bc46f7d5bbfd2698078e2f06af450067
JaimeLxrd <jf847852208@gmail.com>
2026-06-13 02:19:07 +0200

    Add next-auth dependency

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -7,6 +7,9 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  outputFileTracingIncludes: {
+    "/**": ["../../packages/database/generated/prisma/**/*"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
